39af37da1a540769d068d52ed36b1140338e3a50
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-26 21:24:48 +0100

    Add windows configure.bat

---
+++ configure.bat
@@ -0,0 +1,5 @@
+@set build_type=Release
+@if not "%1"=="" set build_type=%1
+@echo Configuring for build type %build_type%
+@set INCLUDE=%cd%\..\..\FreeImage\Dist;%INCLUDE%
+cmake -G "NMake Makefiles" -DBOOST_ROOT:STRING="..\boost_1_56_0" -DBOOST_LIBRARYDIR:STRING="..\boost_1_56_0\lib64-msvc-12.0" -DCMAKE_INSTALL_PREFIX="install/%build_type%" -DCMAKE_BUILD_TYPE="%build_type%" ..
