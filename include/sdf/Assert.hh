a33912f5f5e6047b7c8ab8e406f1f99e5ae5eb2c
Chris Lalancette <clalancette@osrfoundation.org>
2017-02-13 14:52:33 -0500

    Get rid of BOOST_ASSERT.
    Instead, make SDF_ASSERT throw an exception.  This is different
    behavior than what BOOST_ASSERT does by default, but it makes
    more sense for a library to throw an exception rather than
    abort the whole program.

--- include/sdf/Assert.hh
+++ include/sdf/Assert.hh
@@ -17,10 +17,10 @@
 #ifndef _SDF_ASSERT_HH_
 #define _SDF_ASSERT_HH_
 
-#include <boost/assert.hpp>
+#include "sdf/Exception.hh"
 
 /// \brief This macro define the standard way of launching an exception
-/// inside gazebo.
-#define SDF_ASSERT(_expr, _msg) BOOST_ASSERT_MSG(_expr, _msg)
+/// inside SDF.
+#define SDF_ASSERT(_expr, _msg) (__builtin_expect(!!(_expr), 1) ? ((void)0) : throw sdf::AssertionInternalError(__FILE__, __LINE__, #_expr, __FUNCTION__, _msg))  // NOLINT(whitespace/line_length)
 
 #endif
